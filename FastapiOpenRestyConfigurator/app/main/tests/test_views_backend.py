81ef9b5983a8efab766080c8953b5b51b29b784a
Milad Tajdar <milad.tajdar@hotmail.com>
2026-01-20 09:12:15 +0000

    testing environment, added some test functions

--- FastapiOpenRestyConfigurator/app/main/tests/test_views_backend.py
+++ FastapiOpenRestyConfigurator/app/main/tests/test_views_backend.py
@@ -1,72 +1,46 @@
 import pytest
 from unittest.mock import patch
-
-from app.main.model.serializers import BackendOut
+from fastapi import HTTPException
 
 from app.main.views import backend as backend_views
 
-from fastapi import HTTPException
 
-
+@pytest.mark.parametrize(
+    "exception_expected, backend_id, body",
+    [
+        (False, 123, {"auth_enabled": True}),
+        # success cases
+        (False, 123, {"auth_enabled": 1}),
+        (False, 123, {"auth_enabled": 0}),
+        (False, 123, {"auth_enabled": False}),
+        # corrupted backend_id
+        # TODO: more tests when there is further validation on backend_id
+        (True, "not an int", {"auth_enabled": True}),
+        (True, None, {"auth_enabled": True}),
+        # corrupted body
+        (True, 123, {"auth_enabled": "not a boolean"}),
+        (True, 123, {"auth_enabled": ""}),
+        (True, 123, {"differrent_value": True}),
+        (True, 123, None),
+        (True, 123, {}),
+    ]
+)
 @pytest.mark.asyncio
-async def test_backend_update_auth_activate():
+async def test_backend_update_auth(exception_expected, backend_id, body):
 
     with patch(
-        "app.main.views.backend.update_backend_authorization",
-        return_value = BackendOut(
-            backend_id = 123,
-            auth_enabled = True,
-        )
+        "app.main.service.backend.update_backend_authorization"
     ) as mock_update_backend_authorization:
-
-        response: BackendOut = await backend_views.backend_update_auth(
-            backend_id = 123,
-            body = {"auth_enabled": True},
-            api_key = "test"
-        )
-
-        assert response.backend_id == 123
-        assert response.auth_enabled is True
-        mock_update_backend_authorization.assert_awaited_once_with(backend_id = 123, auth_enabled = True)
-
-
-@pytest.mark.asyncio
-async def test_backend_update_auth_deactivate():
-
-    with patch(
-        "app.main.views.backend.update_backend_authorization",
-        return_value = BackendOut(
-            backend_id = 123,
-            auth_enabled = False,
-        )
-    ) as mock_update_backend_authorization:
-
-        response: BackendOut = await backend_views.backend_update_auth(
-            backend_id = 123,
-            body = {"auth_enabled": False},
-            api_key = "test"
-        )
-
-        assert response.backend_id == 123
-        assert response.auth_enabled is False
-        mock_update_backend_authorization.assert_awaited_once_with(backend_id = 123, auth_enabled = False)
-
-
-@pytest.mark.asyncio
-async def test_backend_update_auth_invalid_body():
-
-    with pytest.raises(HTTPException) as not_boolean_exception:
-        await backend_views.backend_update_auth(
-            backend_id = 123,
-            body = {"auth_enabled": "not a boolean"},
-            api_key = "test"
-        )
-    assert not_boolean_exception.value.status_code == 422
-
-    with pytest.raises(HTTPException) as empty_body_exception:
-        await backend_views.backend_update_auth(
-            backend_id = 123,
-            body = {},
-            api_key = "test"
-        )
-    assert empty_body_exception.value.status_code == 422
+        # success case
+        try:
+            await backend_views.backend_update_auth(
+                backend_id = backend_id,
+                body = body,
+                api_key = object() # type: ignore[reportArgumentType]
+            )
+            mock_update_backend_authorization.assert_called_once_with(backend_id = 123, auth_enabled = True)
+        # fail case
+        except Exception:
+            if exception_expected:
+                mock_update_backend_authorization.assert_not_awaited()
+        #if not exception_expected:
--- FastapiOpenRestyConfigurator/app/main/views/backend.py
+++ FastapiOpenRestyConfigurator/app/main/views/backend.py
@@ -53,19 +53,27 @@ async def create_backend(backend_in: BackendIn, api_key: APIKey = Depends(get_ap
     summary="Set owner authorization to true/false for an existing backend."
 )
 async def backend_update_auth(backend_id: int, body: dict = Body(...), api_key: APIKey = Depends(get_api_key)):
-    backend_id = int(secure_filename(str(backend_id))) # TODO: do we need secure_filename for int?
-    enable_auth = body.get("auth_enabled", None)
-    logger.debug(f"Updating backend authorization to {enable_auth} for backend id: {backend_id}")
-    if enable_auth is None or not isinstance(enable_auth, bool):
-        logger.error(f"auth_enabled is required and must be a boolean, backend_id: {backend_id}, auth_enabled: {enable_auth}, {type(enable_auth)}")
-        raise HTTPException(status_code=422,
-                            detail=f"auth_enabled is required and must be a boolean, backend_id: {backend_id}, auth_enabled: {enable_auth}, {type(enable_auth)}")
-    try:
-        return await backend_service.update_backend_authorization(backend_id, enable_auth)
-    except NotFound:
-        raise HTTPException(status_code=404, detail=f"Backend with id {backend_id} not found.")
-    except InternalServerError:
-        raise HTTPException(status_code=500, detail="Internal server error.")
+    # process inputs TODO: should we validate further?
+    backend_id = int(secure_filename(str(backend_id))) # TODO: are secure_filename and str necessary? validation?
+    enable_auth = bool(body.get("auth_enabled", None))
+    logger.debug(f"Attempting to update backend authorization to {enable_auth} for backend id: {backend_id}")
+
+    # check inputs and raise error
+    if backend_id is None or enable_auth is None or not isinstance(enable_auth, bool):
+        logger.error(
+            f"Received faulty data. backend_id: {backend_id}, auth_enabled: {enable_auth}, {type(enable_auth)}")
+        raise HTTPException(status_code=422, detail =
+                            f"auth_enabled is required and must be a boolean, \
+                                backend_id: {backend_id}, auth_enabled: {enable_auth}, {type(enable_auth)}")
+    # forward to service layer
+    else:
+        try:
+            return await backend_service.update_backend_authorization(backend_id, enable_auth)
+        # TODO: the exceptions are raised in the service layer already, do we still need this?
+        except NotFound:
+            raise HTTPException(status_code=404, detail=f"Backend with id {backend_id} not found.")
+        except InternalServerError:
+            raise HTTPException(status_code=500, detail="Internal server error.")
 
 
 @router.get(
